60b78c87a515e951696791b5f866851bcdb70a56
Your Name <you@example.com>
2021-07-27 12:19:03 -0500

    current day values

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,13 @@ var appid = 'c37f222d1be87273f5169d1997df3ab3';
 var cityNameEl = document.getElementById('city');
 var temperature = document.getElementById('temp');
 var wind = document.getElementById('wind');
+var humidity = document.getElementById('humidity');
+var currentDay = moment().format('d/MM/YYYY');
+var iconEl = document.getElementById('icon');
+var uvEl = document.getElementById('uv');
 
 var getSingleForecast = function(){
+    ///fetch call to get everything but UV value
     var searchCity = document.getElementById('input').value;
     var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + 
     searchCity +
@@ -18,14 +23,31 @@ var getSingleForecast = function(){
         //if request was successful 
         if(response.ok){
             response.json().then(function(data){
-                console.log(data.name);
-                cityNameEl.textContent = data.name; //add moment data here;
+                var logo = data.weather[0].icon;
+                iconEl.setAttribute("src","https://openweathermap.org/img/wn/" + logo + "@2x.png");
+                iconEl.setAttribute("alt", data.weather[0].description)
+
+                cityNameEl.textContent = data.name + ' ' + currentDay;
                 console.log(data);
-                console.log(data.main)
                 temperature.textContent = 'Temperature: ' + data.main.temp; // add F for temp
                 wind.textContent = 'Wind: ' + data.wind.speed + ' MPH'
-                console.log(data.wind.speed);
+                humidity.textContent = 'Humidity: ' + data.main.humidity + '%'
+                console.log(data.weather[0].icon);
+                
+                //fetch call for UV value
+                var uvApi = 'https://api.openweathermap.org/data/2.5/uvi/forecast?lat='+
+                data.coord.lat + '&lon=' + data.coord.lon + '&appid=' + appid +
+                '&cnt=1';
+                fetch(uvApi)
+                .then(function(response){
+                    if(response.ok){
+                        response.json().then(function(data){
+                            uvEl.innerHTML ='UV-Index: ' + data[0].value;
+                        })
+                    }
+                })
             });
+            
         }else{
             alert('Somethings wrong with the request')
         } 
@@ -36,4 +58,3 @@ var getSingleForecast = function(){
 };
 
 buttonEl.addEventListener('click',getSingleForecast);
-
