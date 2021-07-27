ce69237c290e85cfa9c9fbf4642f9f9aada6039b
Your Name <you@example.com>
2021-07-27 14:42:24 -0500

    obtained info in obj for 5day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,8 @@ var humidity = document.getElementById('humidity');
 var currentDay = moment().format('d/MM/YYYY');
 var iconEl = document.getElementById('icon');
 var uvEl = document.getElementById('uv');
+var card1 = document.getElementById('card1');
+
 var getSingleForecast = function(){
     ///fetch call to get everything but UV value
     var searchCity = document.getElementById('input').value;
@@ -27,7 +29,7 @@ var getSingleForecast = function(){
                 iconEl.setAttribute("alt", data.weather[0].description)
 
                 cityNameEl.textContent = data.name + ' ' + currentDay;
-                console.log(data);
+                // console.log(data);
                 temperature.textContent = 'Temperature: ' + data.main.temp; // add F for temp
                 wind.textContent = 'Wind: ' + data.wind.speed + ' MPH'
                 humidity.textContent = 'Humidity: ' + data.main.humidity + '%'
@@ -56,39 +58,70 @@ var getSingleForecast = function(){
     });  
 };
 
+//function to request 5-day forecast
 var fiveDayForecast = function(){
-    //call to get id
     var searchCity = document.getElementById('input').value;
-    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + 
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + 
     searchCity +
     '&units=imperial' +
     '&appid=' + appid
+
     fetch(apiUrl)
     .then(function(response){
         if(response.ok){
             response.json().then(function(data){
-                var id = (data.id);
-                console.log(id);
-                //call to get 5-day forecast 
-                var apiFive = 'https://api.openweathermap.org/data/2.5/forecast?id='+
-                id + '&appid=' + appid
+                //assign variables to each day 
+                console.log(data);
+                var fiveCast = [data.list[1], data.list[9],data.list[17], data.list[25],data.list[33]];
+                console.log(fiveCast[0].main.temp);
 
-                fetch(apiFive)
-                .then(function(resp){
-                    if(resp.ok){
-                        resp.json().then(function(dat){
-                            console.log(dat);
-                        })
-                    }
-                })
+                card1.innerHTML = 
+                '<h6></h6>' + currentDay + '<br></br>' +
+                '<p>Temp: </p>' + fiveCast[0].main.temp  ;
+
+                var day1= data.list[1];
+                console.log(day1);
+                //card1.innerHTML = data.list[1]
+              
             })
         }
     })
-    
 }
+
+
 
 buttonEl.addEventListener('click',() => {
     fiveDayForecast();
     getSingleForecast();
 });
 
+// var fiveDayForecast = function(){
+//     //call to get id
+//     var searchCity = document.getElementById('input').value;
+//     var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + 
+//     searchCity +
+//     '&units=imperial' +
+//     '&appid=' + appid
+//     fetch(apiUrl)
+//     .then(function(response){
+//         if(response.ok){
+//             response.json().then(function(data){
+//                 var id = (data.id);
+//                 console.log(id);
+//                 //call to get 5-day forecast 
+//                 var apiFive = 'https://api.openweathermap.org/data/2.5/forecast?id='+
+//                 id + '&appid=' + appid
+
+//                 fetch(apiFive)
+//                 .then(function(resp){
+//                     if(resp.ok){
+//                         resp.json().then(function(dat){
+//                             console.log(dat);
+//                         })
+//                     }
+//                 })
+//             })
+//         }
+//     })
+    
+// }
